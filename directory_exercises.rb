70d83a9232c29a03654da74737f2af97faa63175
samcolson4 <samcolson4@gmail.com>
2020-08-24 14:52:18 +0100

    Updated exercises to include those up to no.11

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -9,7 +9,7 @@ def input_students
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
 
-    name = gets.chomp
+    name = gets.strip
   end
   students
 end
@@ -28,6 +28,41 @@ def input_students_more
     height = gets.chomp
 
     students << {name: name, cohort: :november, hobby: hobby, height: height}
+    puts "Now we have #{students.count} students"
+
+    puts "Please enter the names of the student:"
+    puts "To finish, just return twice"
+    name = gets.chomp
+
+  end
+  students
+end
+
+def input_students_cohort
+  # using November as the default cohort.
+
+  students = []
+  cohort_options = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+  puts "Please enter the names of the student:"
+  puts "To finish, just return twice"
+  name = gets.chomp
+
+  while !name.empty? do
+    puts "Please enter the cohort of the student:"
+    cohort = gets.chomp
+
+    if cohort == ""
+      students << {name: name, cohort: :november}
+    elsif cohort_options.include?(cohort)
+      students << {name: name, cohort: cohort.to_sym}
+    else
+      puts "Please type one of the months as displayed here to enter a cohort"
+      puts cohort_options
+      cohort = gets.chomp
+      students << {name: name, cohort: cohort.to_sym}
+    end
+
     puts "Now we have #{students.count} students"
 
     puts "Please enter the names of the student:"
@@ -81,7 +116,63 @@ def print_while(students) # old print + index number
     puts "#{new_list[counter].to_s}"
     counter += 1
   end
+end
 
+def print_cohorts_ask(students)
+  puts "Which cohort would you like to print?"
+  cohort_month = gets.chomp
+
+  puts "The students from the #{cohort_month} cohort are:"
+    students.each { |student, cohort|
+      if student[:cohort] == cohort_month.to_sym
+        print_cohort = []
+        print_cohort << student
+        print_cohort.each { |student| puts "#{student[:name]}" }
+      end
+    }
+end
+
+=begin
+def print_cohorts_all(students)
+  cohort_options = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+  counter = 0
+  month = cohort_options[counter]
+  out_month = []
+
+  while counter <= cohort_options.count
+    puts "The students in the #{month} are:"
+
+    students.each { |student, group|
+      if students[:cohort].to_s == month
+        out_month << student[:name]
+      end
+    }
+
+    puts out_month
+    counter += 1
+  end
+end
+=end
+
+def print_group_cohorts(students) #prints the cohorts sperated into groups
+
+  sort_by_months = {}
+
+  students.each do |student|
+    cohort = student[:cohort].to_s
+
+    if sort_by_months[cohort] == nil
+      sort_by_months[cohort] = []
+    end
+
+    sort_by_months[cohort].push(student[:name])
+  end
+
+  sort_by_months.each do |cohort|
+    puts cohort
+    students.each { |student| puts student[:name] if student[cohort] == cohort}
+  end
 end
 
 def print_footer(students)
@@ -94,11 +185,15 @@ end
 
 
 # students = input_students
-students = input_students_more
+# students = input_students_more
+students = input_students_cohort
 print_header
 # print(students)
-print_more(students)
+# print_more(students)
 # print_f(students)
 # print_under_twelve(students)
 # print_while(students)
+# print_cohorts_ask(students)
+# print_cohorts_all(students)
+print_group_cohorts(students)
 print_footer(students)
