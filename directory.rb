798ea426fc524aa12cf245c1e6a64d7343654a42
samcolson4 <samcolson4@gmail.com>
2020-08-24 10:13:45 +0100

    Updated directory.rb to take user input

--- directory.rb
+++ directory.rb
@@ -1,5 +1,6 @@
 # let's put all students into an array
-students = [
+=begin
+old_students = [
   {name: "Dr. Hannibal Lecter", cohort: :november},
   {name: "Darth Vader", cohort: :november},
   {name: "Nurse Ratched", cohort: :november},
@@ -12,7 +13,23 @@ students = [
   {name: "Joffrey Baratheon", cohort: :november},
   {name: "Norman Bates", cohort: :november}
 ]
+=end
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just return twice"
+
+  students = []
+  name = gets.chomp
+
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+
+    name = gets.chomp
+  end
+  students
+end
 
 def print_header
   puts "The students of Villains Academy"
@@ -29,6 +46,7 @@ def print_footer(students)
     puts "Overall, we have #{students.count} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
