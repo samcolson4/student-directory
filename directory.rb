eeb5890feafd1b7664cb1cffb5641ce4fd30f68f
samcolson4 <samcolson4@gmail.com>
2020-08-25 08:46:08 +0100

    Added interactive menu

--- directory.rb
+++ directory.rb
@@ -1,19 +1,27 @@
-# let's put all students into an array
-=begin
-old_students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
-=end
+def interactive_menu
+  students = []
+
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+
+    selection = gets.chomp
+
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, please try again."
+    end
+  end
+end
 
 def input_students
   puts "Please enter the names of the students"
@@ -46,7 +54,4 @@ def print_footer(students)
     puts "Overall, we have #{students.count} great students"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
