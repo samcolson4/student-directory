0fc31ce9cb65fff4859ae544e23d4f514dbb4067
samcolson4 <samcolson4@gmail.com>
2020-08-25 09:13:19 +0100

    Updated directory.rb to allow saving of files. Added '.gitignore' to make sure student names aren't uploaded

--- directory.rb
+++ directory.rb
@@ -1,37 +1,5 @@
 @students = []
 
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
-  end
-end
-
-def process(selection)
-  case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "9"
-      exit
-    else
-      puts "I don't know what you mean, please try again."
-  end
-end
-
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "9. Exit"
-end
-
-def show_students
-  print_header
-  print
-  print_footer
-end
-
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just return twice"
@@ -46,12 +14,47 @@ def input_students
   end
 end
 
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit" #9 because more items will be added.
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "3"
+      save_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, please try again."
+  end
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "---------"
 end
 
-def print
+def print_student_list
   @students.each { |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   }
@@ -61,4 +64,47 @@ def print_footer
     puts "Overall, we have #{@students.count} great students"
 end
 
+def save_students
+ # open the file for writing
+ file = File.open("students.csv", "w")
+ # iterate over the array of students
+ @students.each { |student|
+   student_data = [student[:name], student[:cohort]]
+   csv_line = student_data.join(",")
+   file.puts csv_line
+ }
+ file.close
+end
+
+
+
+
 interactive_menu
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###
